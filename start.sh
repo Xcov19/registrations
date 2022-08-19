843b2ee04e5b3523210fb51818e62b0ec2cce0b6
codecakes <akulmat@protonmail.com>
2022-08-19 03:12:52 +0200

    prod and test version if else

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-python3 -m poetry run hypercorn -k uvloop --bind="0.0.0.0:8080" --debug --reload registrations.infrastructure.adapters.api.app:app
+if [ "$ENV" == "test" ];
+then
+  python3 -m poetry run hypercorn -k uvloop --bind="0.0.0.0:${LOCAL_PORT}" --debug --reload registrations.infrastructure.adapters.api.app:app
+else
+  python3 -m poetry run hypercorn -k uvloop --bind="0.0.0.0:${LOCAL_PORT}" --reload registrations.infrastructure.adapters.api.app:app
+fi;
